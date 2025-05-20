76cb48d9d75d8edfc32583f16d9d9f795b0988e6
Amaris <amarisosorio10@gmail.com>
2025-05-20 06:01:50 -0600

    No me funciona el useContext

---
+++ Practica-Context/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Information from "./pages/Informacion";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path= "/" element= { <Login /> } />
+            <Route path= "/" element= { <PrivateRoute /> }>
+               <Route path= "/" element= { <Dashboard/> } />
+            </Route>
+             <Route path= "/" element= { <Information/> } />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  )
+};
+
+
+export default App;
